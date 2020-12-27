d957653a7375825df16fae71248d6827177cd9de
tim <wright.tim.j@gmail.com>
2020-12-27 16:41:58 +0000

    issues with startup, yml file

---
+++ build.sh
@@ -0,0 +1,23 @@
+
+mvn -f predictor-scraper-reactive/pom.xml clean package && docker build -t timmytime/predictor-scraper-reactive predictor-scraper-reactive/
+mvn -f predictor-event-scraper-reactive/pom.xml clean package && docker build -t timmytime/predictor-event-scraper-reactive predictor-event-scraper-reactive/
+mvn -f predictor-data-reactive/pom.xml clean package && docker build -t timmytime/predictor-data-reactive predictor-data-reactive/ 
+mvn -f predictor-event-data-reactive/pom.xml clean package && docker build -t timmytime/predictor-event-data-reactive predictor-event-data-reactive/ 
+mvn -f predictor-teams-reactive/pom.xml clean package && docker build -t timmytime/predictor-teams-reactive predictor-teams-reactive/ 
+mvn -f predictor-events-reactive/pom.xml clean package && docker build -t timmytime/predictor-events-reactive predictor-events-reactive/
+mvn -f predictor-players-reactive/pom.xml clean package && docker build -t timmytime/predictor-players-reactive predictor-players-reactive/
+mvn -f predictor-players-events-reactive/pom.xml clean package && docker build -t timmytime/predictor-players-events-reactive predictor-players-events-reactive/
+mvn -f predictor-client-reactive/pom.xml clean package && docker build -t timmytime/predictor-client-reactive predictor-client-reactive/
+docker push timmytime/predictor-scraper-reactive
+docker push timmytime/predictor-event-scraper-reactive
+docker push timmytime/predictor-data-reactive
+docker push timmytime/predictor-event-data-reactive
+docker push timmytime/predictor-teams-reactive
+docker push timmytime/predictor-events-reactive
+docker push timmytime/predictor-players-reactive
+docker push timmytime/predictor-players-events-reactive
+docker push timmytime/predictor-client-reactive
+
+
+
+
