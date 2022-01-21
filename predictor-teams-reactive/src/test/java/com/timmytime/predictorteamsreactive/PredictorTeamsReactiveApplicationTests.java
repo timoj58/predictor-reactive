325da36f66566516b5aabda4f6788b4e2a83ade1
tim <wright.tim.j@gmail.com>
2022-01-21 14:40:40 +0000

    trigger

--- predictor-teams-reactive/src/test/java/com/timmytime/predictorteamsreactive/PredictorTeamsReactiveApplicationTests.java
+++ predictor-teams-reactive/src/test/java/com/timmytime/predictorteamsreactive/PredictorTeamsReactiveApplicationTests.java
@@ -2,8 +2,6 @@ package com.timmytime.predictorteamsreactive;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 class PredictorTeamsReactiveApplicationTests {
 
