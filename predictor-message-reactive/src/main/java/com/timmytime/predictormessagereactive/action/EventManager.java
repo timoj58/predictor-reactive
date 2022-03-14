5fae9f6afa853ecef06ab18c58b281e0cf6007a8
tim <wright.tim.j@gmail.com>
2022-03-14 18:46:14 +0000

    remove init step

--- predictor-message-reactive/src/main/java/com/timmytime/predictormessagereactive/action/EventManager.java
+++ predictor-message-reactive/src/main/java/com/timmytime/predictormessagereactive/action/EventManager.java
@@ -2,7 +2,7 @@ package com.timmytime.predictormessagereactive.action;
 
 import com.timmytime.predictormessagereactive.action.event.IEventAction;
 import com.timmytime.predictormessagereactive.enumerator.Action;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -10,18 +10,17 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@RequiredArgsConstructor
 public class EventManager {
     private final Map<Action, EventAction> eventManager = new ConcurrentHashMap<>();
-    private final Collection<IEventAction> eventActions;
 
-    public void init() {
-
+    @Autowired
+    public EventManager(
+            Collection<IEventAction> eventActions
+    ){
         eventActions.forEach(impl -> {
             var created = impl.create();
             eventManager.put(created.getLeft(), created.getRight());
         });
-
 
     }
 
