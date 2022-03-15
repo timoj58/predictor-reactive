b5d2b705717a1509444c93a4ddce41772005f7b0
tim <wright.tim.j@gmail.com>
2022-03-15 10:47:04 +0000

    inc wait due to slow github test

--- predictor-message-reactive/src/test/java/com/timmytime/predictormessagereactive/service/impl/OrchestrationServiceImplTest.java
+++ predictor-message-reactive/src/test/java/com/timmytime/predictormessagereactive/service/impl/OrchestrationServiceImplTest.java
@@ -61,7 +61,7 @@ class OrchestrationServiceImplTest {
                         ).build()
         );
 
-        Thread.sleep(200);
+        Thread.sleep(250);
 
         verify(predictorCycleRepo, atLeastOnce()).save(any());
 
@@ -82,7 +82,7 @@ class OrchestrationServiceImplTest {
                         ).build()
         );
 
-        Thread.sleep(100);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeast(2)).scrape(anyString());
 
@@ -102,7 +102,7 @@ class OrchestrationServiceImplTest {
                                 ).build()
                 ));
 
-        Thread.sleep(200);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeastOnce()).finish(anyString(), any());
 
@@ -121,7 +121,7 @@ class OrchestrationServiceImplTest {
                                 ).build()
                 ));
 
-        Thread.sleep(200);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeast(EventType.countries().size())).train(anyString(), any());
 
@@ -152,7 +152,7 @@ class OrchestrationServiceImplTest {
                         ).build()
         );
 
-        Thread.sleep(100);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeast(EventType.countries().size())).predict(anyString(), any());
     }
@@ -171,7 +171,7 @@ class OrchestrationServiceImplTest {
                                 ).build()
                 ));
 
-        Thread.sleep(100);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeast(EventType.countries().size())).train(anyString(), any());
 
@@ -204,7 +204,7 @@ class OrchestrationServiceImplTest {
                                         ).build()
                         ));
 
-        Thread.sleep(100);
+        Thread.sleep(250);
 
         verify(webClientFacade, atLeast(EventType.countries().size())).predict(anyString(), any());
 
