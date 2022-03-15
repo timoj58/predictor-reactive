37cada13d9a38b49016a647980c92b0d1420fdc3
tim <wright.tim.j@gmail.com>
2022-03-15 16:16:37 +0000

    more tests, sort of pointless just to make badges changes, branches more important

--- predictor-teams-reactive/src/test/java/com/timmytime/predictorteamsreactive/service/impl/TrainingHistoryServiceImplTest.java
+++ predictor-teams-reactive/src/test/java/com/timmytime/predictorteamsreactive/service/impl/TrainingHistoryServiceImplTest.java
@@ -1,17 +1,19 @@
 package com.timmytime.predictorteamsreactive.service.impl;
 
+import com.timmytime.predictorteamsreactive.enumerator.CountryCompetitions;
 import com.timmytime.predictorteamsreactive.enumerator.Training;
 import com.timmytime.predictorteamsreactive.model.TrainingHistory;
 import com.timmytime.predictorteamsreactive.repo.TrainingHistoryRepo;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class TrainingHistoryServiceImplTest {
 
@@ -43,5 +45,36 @@ class TrainingHistoryServiceImplTest {
 
         assertNotNull(trainingHistoryService.find(type, country));
 
+    }
+
+    @Test
+    void nextTest(){
+
+        when(trainingHistoryRepo.findByTypeAndCountryOrderByDateDesc(
+                Training.TRAIN_GOALS, "england"
+        )).thenReturn(List.of(TrainingHistory.builder()
+                .toDate(LocalDateTime.now()).build()));
+
+        when(trainingHistoryRepo.save(any())).thenReturn(
+                TrainingHistory.builder().build()
+        );
+
+        var res = trainingHistoryService.next(
+                Training.TRAIN_GOALS, "england", 1
+        );
+
+        assertNotNull(res);
+
+        verify(trainingHistoryRepo, atLeastOnce()).save(any());
+    }
+
+    @Test
+    void initTest(){
+
+        trainingHistoryService.init("01-01-2011", "05-01-2011");
+
+        verify(trainingHistoryRepo, atLeast(
+                CountryCompetitions.values().length * 2
+        )).save(any());
     }
 }
